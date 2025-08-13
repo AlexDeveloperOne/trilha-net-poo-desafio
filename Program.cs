a4795194ce2e765c6e3e19a8a630344ef4752805
Alexandre A. Silva <alexdroide.developer@gmail.com>
2025-08-13 12:25:35 -0300

    test(program): implementar testes das classes Smartphone, iPhone e Nokia
    Criados exemplos no Program.cs para testar funcionalidades das classes
    Smartphone, iPhone e Nokia, incluindo métodos Ligar, ReceberLigacao
    e InstalarAplicativo.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 6.1", "123456789012345", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
